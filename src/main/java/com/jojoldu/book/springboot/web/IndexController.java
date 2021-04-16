2d6df21c7e64ec1231f94813f1ac016f2eafdcc6
songseongbin <ppsosqq@gmail.com>
2021-04-16 19:43:31 +0900

    4.4 전체 조회 화면

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,12 +1,22 @@
 package com.jojoldu.book.springboot.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.jojoldu.book.springboot.service.posts.PostsService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+	private final PostsService postsService;
+
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("posts", postsService.findAllDesc());
 		return "index";
 	}
 
