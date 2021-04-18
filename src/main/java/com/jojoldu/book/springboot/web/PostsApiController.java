9419feb23bf3c904184b735f5fe89dd0f3dc3e7f
songseongbin <ppsosqq@gmail.com>
2021-04-18 18:38:59 +0900

    4.5 게시글 삭제

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,4 +36,9 @@ public class PostsApiController {
 		return postsService.findById(id);
 	}
 
+	@DeleteMapping("/api/v1/posts/{id}")
+	public Long delete(@PathVariable Long id) {
+		postsService.delete(id);
+		return id;
+	}
 }
